83638dc5b8b9a261f4efa38e4bf642b5d1843a61
AbduGad <abdo79gad@gmail.com>
2024-06-24 20:57:57 +0300

    "Updated pagination helper function and added deletion-resilient hypermedia pagination class"

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,5 +9,4 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple containing a start and end index
     """
-
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
