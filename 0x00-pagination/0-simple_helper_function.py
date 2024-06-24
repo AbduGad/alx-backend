de5b2feeeb3b83f4e5c95da8ba2e98a0d238a98c
AbduGad <abdo79gad@gmail.com>
2024-06-24 17:04:18 +0300

    back tracking the last commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,5 +9,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple containing a start and end index
     """
-    nextPageStartIndex = page * page_size
-    return nextPageStartIndex - page_size, nextPageStartIndex
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
