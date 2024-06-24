0aa0986f181a1a9aa7ed59287fc5f67532ad82a9
AbduGad <abdo79gad@gmail.com>
2024-06-24 16:16:34 +0300

    "Added pagination helper function with Flask and pandas imports"

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Pagination helper function.
+"""
+from flask import Flask, request, jsonify
+import pandas
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+	"""Return a tuple containing a start and end index
+	"""
+	
+	return ((page - 1) * page_size , ((page - 1) * page_size) + page_size)
