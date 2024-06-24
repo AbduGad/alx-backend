122ea8dfaa433f1d9566bdc37154d445957dbcd7
AbduGad <abdo79gad@gmail.com>
2024-06-24 16:44:37 +0300

    "Indentation fix in docstring of index_range function"

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-	"""Return a tuple containing a start and end index
-	"""
+    """Return a tuple containing a start and end index
+    """
 
-	return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
